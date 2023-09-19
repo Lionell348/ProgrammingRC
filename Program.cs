4ee638f06bb7f85981503d943ac9dfed197e2d07
Lionell348 <111586584+Lionell348@users.noreply.github.com>
2023-09-19 15:22:18 +0200

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,234 +1,161 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Media;
-using System.Windows.Shapes;
-using System.Windows.Controls.DataVisualization.Charting;
 
-namespace RecipeApplication
+namespace TimeManagementApp
 {
+    public class Module
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int Credits { get; set; }
+        public int ClassHoursPerWeek { get; set; }
+        public int SelfStudyHoursPerWeek { get; set; }
+        public Dictionary<DateTime, int> StudyHoursRecord { get; set; }
+
+        public Module()
+        {
+            StudyHoursRecord = new Dictionary<DateTime, int>();
+        }
+    }
+
     public partial class MainWindow : Window
     {
-        private RecipeManager recipeManager;
-        private List<Recipe> filteredRecipes;
+        private List<Module> modules = new List<Module>();
+        private int numberOfWeeks;
+        private DateTime startDate;
+        private object ModuleCodeTextBox;
+        private object ModuleClassHoursTextBox;
+        private readonly object ModuleNameTextBox;
+
+        public object ModuleCreditsTextBox { get; private set; }
+        public object HoursStudiedTextBox { get; private set; }
+        public object ModuleListBox { get; private set; }
 
         public MainWindow()
         {
             InitializeComponent();
-
-            recipeManager = new RecipeManager();
-            // Add some sample recipes for testing
-            recipeManager.AddRecipe(new Recipe("Recipe 1"));
-            recipeManager.AddRecipe(new Recipe("Recipe 2"));
-            recipeManager.AddRecipe(new Recipe("Recipe 3"));
-
-            recipeListBox.ItemsSource = recipeManager.Recipes;
-
-            filteredRecipes = new List<Recipe>();
         }
 
-        private void AddRecipeButton_Click(object sender, RoutedEventArgs e)
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void AddModule_Click(object sender, RoutedEventArgs e)
         {
             
-            AddRecipeWindow addRecipeWindow = new AddRecipeWindow();
-            addRecipeWindow.ShowDialog();
 
-            // After the dialog is closed, check if a recipe was added
-            if (addRecipeWindow.DialogResult.HasValue && addRecipeWindow.DialogResult.Value)
+            Module newModule = new Module
             {
-                Recipe newRecipe = addRecipeWindow.GetRecipe();
-                recipeManager.AddRecipe(newRecipe);
-                filteredRecipes.Add(newRecipe);
-                recipeListBox.ItemsSource = filteredRecipes;
-            }
+                Code = ModuleCodeTextBox.Text,
+                Name = ModuleNameTextBox.Text,
+                Credits = int.Parse(ModuleCreditsTextBox.Text),
+                ClassHoursPerWeek = int.Parse(ModuleClassHoursTextBox.Text),
+                SelfStudyHoursPerWeek = 0,
+            };
+
+            modules.Add(newModule);
+
+            
+
+            RefreshModuleList();
         }
 
-        private void ViewDetailsButton_Click(object sender, RoutedEventArgs e)
+        private void CalculateSelfStudyHours_Click(object sender, RoutedEventArgs e)
         {
-            Recipe selectedRecipe = recipeListBox.SelectedItem as Recipe;
-            if (selectedRecipe != null)
+           
+
+            foreach (Module module in modules)
             {
+                module.SelfStudyHoursPerWeek = (module.Credits * 10) - module.ClassHoursPerWeek;
                 
-                RecipeDetailsWindow recipeDetailsWindow = new RecipeDetailsWindow(selectedRecipe);
-                recipeDetailsWindow.ShowDialog();
+
             }
-            else
+        }
+
+        private void RecordStudyHours_Click(object sender, RoutedEventArgs e, object DatePicker)
+        {
+            
+            Module selectedModule = GetSelectedModule();
+
+            
+            DateTime selectedDate = DatePicker.SelectedDate ?? DateTime.Now; 
+
+            int hoursStudied = int.Parse(HoursStudiedTextBox.Text);
+
+            if (selectedModule != null)
             {
-                MessageBox.Show("Please select a recipe.");
+                if (selectedModule.StudyHoursRecord.ContainsKey(selectedDate))
+                {
+                    selectedModule.StudyHoursRecord[selectedDate] += hoursStudied;
+                }
+                else
+                {
+                    selectedModule.StudyHoursRecord[selectedDate] = hoursStudied;
+                }
+
+                UpdateRemainingSelfStudyHours(selectedModule);
             }
         }
 
-        private void FilterButton_Click(object sender, RoutedEventArgs e)
+        private Module GetSelectedModule()
         {
-            string ingredientFilter = ingredientFilterTextBox.Text;
-            string foodGroupFilter = foodGroupComboBox.Text;
-            double maxCaloriesFilter = 0;
-            double.TryParse(maxCaloriesTextBox.Text, out maxCaloriesFilter);
-
-           
-            filteredRecipes = recipeManager.FilterRecipes(ingredientFilter, foodGroupFilter, maxCaloriesFilter);
-
-            // Update the recipe list box with the filtered recipes
-            recipeListBox.ItemsSource = filteredRecipes;
-
-            // Reset the selected recipe to avoid confusion
-            recipeListBox.SelectedItem = null;
+            throw new NotImplementedException();
         }
 
-        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        private Module GetSelectedModule(object ModuleListBox)
         {
-            
-            if (filteredRecipes.Count > 0)
+            int selectedIndex = ModuleListBox.SelectedIndex;
+            if (selectedIndex >= 0 && selectedIndex < modules.Count)
             {
-                MenuWindow menuWindow = new MenuWindow(filteredRecipes);
-                menuWindow.ShowDialog();
+                return modules[selectedIndex];
             }
-            else
-            {
-                MessageBox.Show("Please filter the recipes first.");
-            }
+
+            return null;
         }
 
-        private void ResetFilterButton_Click(object sender, RoutedEventArgs e)
+
+        private void UpdateRemainingSelfStudyHours(Module selectedModule)
         {
-            
-            ingredientFilterTextBox.Clear();
-            foodGroupComboBox.SelectedIndex = -1;
-            maxCaloriesTextBox.Clear();
+            int totalStudyHoursThisWeek = selectedModule.StudyHoursRecord
+                .Where(pair => IsInCurrentWeek(pair.Key))
+                .Sum(pair => pair.Value);
 
-            filteredRecipes = new List<Recipe>(recipeManager.Recipes);
-            recipeListBox.ItemsSource = filteredRecipes;
-        }
-    }
+            int remainingSelfStudyHours = selectedModule.SelfStudyHoursPerWeek - totalStudyHoursThisWeek;
 
-    public partial class AddRecipeWindow : Window
-    {
-        public AddRecipeWindow()
-        {
-            InitializeComponent();
+            RemainingSelfStudyHoursLabel.Content = $"Remaining Self-Study Hours This Week: {remainingSelfStudyHours} hours";
         }
 
-        public Recipe GetRecipe()
-        {
-            
-            string recipeName = recipeNameTextBox.Text;
-            return new Recipe(recipeName);
-        }
 
-        private void SaveButton_Click(object sender, RoutedEventArgs e)
-        {
-            
-            if (!string.IsNullOrEmpty(recipeNameTextBox.Text))
-            {
-                DialogResult = true;
-                Close();
-            }
-            else
-            {
-                MessageBox.Show("Please enter a recipe name.");
-            }
-        }
-
-        private void CancelButton_Click(object sender, RoutedEventArgs e)
-        {
-            // Close the dialog window without saving the recipe
-            DialogResult = false;
-            Close();
-        }
-    }
-
-    public partial class RecipeDetailsWindow : Window
-    {
-        public RecipeDetailsWindow(Recipe recipe)
-        {
-            InitializeComponent();
-
-            // Display the details of the selected recipe
-            recipeNameLabel.Content = recipe.Name;
-           
-        }
-
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
-        {
-          
-            Close();
-        }
-    }
-
-    public partial class MenuWindow : Window
-    {
-        public MenuWindow(List<Recipe> recipes)
-        {
-            InitializeComponent();
-
-            
-            GenerateMenu(recipes);
-            DisplayPieChart(recipes);
-        }
-
-        private void GenerateMenu(List<Recipe> recipes)
-        {
-            
-            menuListBox.ItemsSource = recipes;
-        }
-
-        private void DisplayPieChart(List<Recipe> recipes)
+        private bool IsInCurrentWeek(DateTime date)
         {
            
-            Chart chart = new Chart();
-            chart.Title = "Recipe Distribution by Food Group";
+            int currentWeekNumber = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(DateTime.Now, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Sunday);
+            int selectedWeekNumber = CultureInfo.CurrentCulture.Calendar.GetWeekOfYear(date, CalendarWeekRule.FirstFourDayWeek, DayOfWeek.Sunday);
 
-            
-            PieSeries pieSeries = new PieSeries();
-            pieSeries.Title = "Food Group";
-            foreach (Recipe recipe in recipes)
-            {
-                pieSeries.Items.Add(new KeyValuePair<string, int>(recipe.FoodGroup, 1));
-            }
-            chart.Series.Add(pieSeries);
-
-            // Add the chart to a container (e.g., Grid) in the window
-            chartContainer.Children.Add(chart);
+            return currentWeekNumber == selectedWeekNumber;
         }
 
-        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        private void RefreshModuleList()
         {
-            // Close the dialog window
-            Close();
+            ModuleListBox.ItemsSource = modules;
         }
+
     }
 
-    public class Recipe
+    internal class RemainingSelfStudyHoursLabel
     {
-        public string Name { get; set; }
-        public string FoodGroup { get; set; }
-
-        public Recipe(string name)
-        {
-            Name = name;
-        }
+        public static string Content { get; internal set; }
     }
 
-    public class RecipeManager
+    internal class RoutedEventArgs
     {
-        public List<Recipe> Recipes { get; }
+    }
 
-        public RecipeManager()
-        {
-            Recipes = new List<Recipe>();
-        }
-
-        public void AddRecipe(Recipe recipe)
-        {
-            Recipes.Add(recipe);
-        }
-
-        public List<Recipe> FilterRecipes(string ingredientFilter, string foodGroupFilter, double maxCaloriesFilter)
-        {
-            
-            return Recipes;
-        }
+    public class Window
+    {
     }
 }
